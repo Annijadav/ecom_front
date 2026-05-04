8111e6520f6a0421766cb565c0089d7db62d29b9
Anil Jadav <niljadavweb@gmail.com>
2026-05-04 22:16:13 +0530

    image fix

--- src/components/categories.js
+++ src/components/categories.js
@@ -58,7 +58,7 @@ const CategoriesSection = () => {
                 src={cat.bannerImage || "/images/D-img.jpg"}
                 alt={cat.name}
                 fill
-                className="object-contain rounded-t"
+                className="object-cover rounded-t"
               />
               <div className="absolute inset-0 bg-black bg-opacity-90 flex items-center justify-center opacity-45 transition-opacity duration-300" />
             </div>
